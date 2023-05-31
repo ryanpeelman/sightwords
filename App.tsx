bae8b6657c402d50939859e80033456d7156ba30
Ryan Peelman <ryan.peelman@gmail.com>
2023-05-30 20:46:23 -0500

    add header component

--- App.tsx
+++ App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import {View} from 'react-native';
 import useWords from './hooks/useWords';
 import Header from './views/Header';
 import WordCarousel from './views/WordCarousel';
+import styles from './styles/styles.css';
 
 export default function App(): JSX.Element {
   const words = useWords();
 
   return (
-    <div>
+    <View style={styles.container}>
       <Header />
       <WordCarousel words={words} />
-    </div>
+    </View>
   );
 }
--- views/Header.tsx
+++ views/Header.tsx
@@ -1,7 +1,21 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {Text, TouchableOpacity, View} from 'react-native';
 import styles from '../styles/styles.css';
 
 export default function Header(): JSX.Element {
-  return <View style={styles.container}></View>;
+  const handle = () => {};
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.button} onPress={handle}>
+        <Text style={styles.buttonText}>All Words</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={handle}>
+        <Text style={styles.buttonText}>By Letter</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={handle}>
+        <Text style={styles.buttonText}>By Count</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
