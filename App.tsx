658c8913c60da0b0d6cd83455b3d6222a5880339
Ryan Peelman <ryan.peelman@gmail.com>
2023-05-30 20:39:24 -0500

    add header component

--- App.tsx
+++ App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import useWords from './hooks/useWords';
+import Header from './views/Header';
 import WordCarousel from './views/WordCarousel';
 
 export default function App(): JSX.Element {
   const words = useWords();
 
-  return <WordCarousel words={words} />;
+  return (
+    <div>
+      <Header />
+      <WordCarousel words={words} />
+    </div>
+  );
 }
---
+++ views/Header.tsx
@@ -0,0 +1,7 @@
+import React, {useState} from 'react';
+import {Text, TouchableOpacity, View} from 'react-native';
+import styles from '../styles/styles.css';
+
+export default function Header(): JSX.Element {
+  return <View style={styles.container}></View>;
+}
